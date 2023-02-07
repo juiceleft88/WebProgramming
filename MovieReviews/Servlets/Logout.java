0cf7e9ba62617408644ddd6233ce35d0ddd52a16
Hugo Izquierdo <70562965+juiceleft88@users.noreply.github.com>
2023-02-07 10:05:48 -0800

    Create Logout.java

---
+++ MovieReviews/Servlets/Logout.java
@@ -0,0 +1,45 @@
+package hw4.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+    public Logout() {
+        super();
+       
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+		HttpSession logOut = request.getSession();
+		
+		//to end the session
+		if(request.getSession(false) != null) {
+			logOut.invalidate();
+		
+		}
+		request.getRequestDispatcher( "/WEB-INF/Logout.jsp" ).forward( request, response );
+		response.sendRedirect("DisplayMovies");
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
